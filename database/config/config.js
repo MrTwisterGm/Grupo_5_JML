26efdafe08801cd561cd33577d79fef6670f4840
Juampi <84481703+Juampi9876@users.noreply.github.com>
2021-09-21 20:02:08 -0300

    ,

--- database/config/config.js
+++ database/config/config.js
@@ -4,7 +4,7 @@ module.exports = {
     "password": null,
     "database": "JML",
     "host": "127.0.0.1",
-    "dialect": "mysql",
+    "dialect": "mysql"
   },
   "test": {
     "username": "root",
