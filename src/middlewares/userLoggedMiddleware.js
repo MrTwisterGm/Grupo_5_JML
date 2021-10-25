34c9032911365ec098e4dd52900e5cedec7cb254
Juampi <84481703+Juampi9876@users.noreply.github.com>
2021-10-25 15:54:22 -0300

    cambios finales

--- src/middlewares/userLoggedMiddleware.js
+++ src/middlewares/userLoggedMiddleware.js
@@ -1,51 +1,42 @@
 const db = require('../../database/models');
-const { Op } = require("sequelize");
-const sequelize = db.sequelize;
 
 
 module.exports = function(req, res, next) {
+    res.locals.userLogged = false;
     if(typeof userToLogin != 'undefined') {
         res.locals.userLogged = req.session.userLogged
+        res.locals.userLogged = true;
     } else if (typeof req.cookies.remember_user != 'undefined') {
-            db.Users.findByPk(req.cookies.remember_user)
-            .then(function(resultado){
-                res.locals.userLogged = resultado;
-                req.session.userLogged = resultado;
-            }).catch(function(e){
-               res.render(e)
-            })
-        }
-    
-
-        next()
+        db.Users.findByPk(req.cookies.remember_user)
+        .then(function(resultado){
+            res.locals.userLogged = resultado;
+            req.session.userLogged = resultado;
+        }).catch(function(e){
+            res.render(e)
+        })
     }
     
+    next()
+}
+
+// function userLoggedMiddleware(req, res, next) {
+//     res.locals.isLogged = false;
+    
+//     let emailInCookie = req.cookies.email;
+//     let userFromCookie = db.Users.findByPk("email", emailInCookie);
+    
+//     if (userFromCookie) {
+//         userToLogin = userFromCookie
+//     }
+    
+//     if (userToLogin) {
+//         res.locals.isLogged = true;
+//         res.locals.userLogged = userToLogin;
+//     }
+    
+//     next();
     
     
-    
-    
-    
-    
-    // async function userLoggedMiddleware(req, res, next) {
-        //     res.locals.isLogged = false;
-        
-        //     let emailInCookie = req.cookies.email;
-        //     let userFromCookie = await db.Users.findOne({
-            //          where:{
-                //             email:{[Op.like]:emailInCookie}
-                //         }})
-                
-                //     if(userFromCookie){
-                    //         req.session.userLogged = userFromCookie;    
-                    //     }
-                    
-                    //     if (req.session.userLogged) {
-                        //         res.locals.isLogged = true;
-                        // 		res.locals.userLogged = req.session.userLogged;
-                        //     } 
-                        
-                        //     next();
-                        
-                        // }
-                        
-                        // module.exports = userLoggedMiddleware;
+// }
+
+// module.exports = userLoggedMiddleware;
